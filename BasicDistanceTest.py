5dba32e96a2641ae1d7893f8cca901043f09a4cd
Spark <spark@Sparks-iMac-11.parkwayschools.net>
2021-11-09 13:44:22 -0600

    Initial commit

---
+++ BasicDistanceTest.py
@@ -0,0 +1,11 @@
+#A TEST for a basic room entering system and a x-cord, y-cord position distance calculator. Not finished
+def enter_room_pos(room, room_x_cord, room_y_cord):
+    print(room, room_x_cord, room_y_cord)
+
+def get_pos(room):
+    print(room)
+
+def get_destination(room):
+    print(room)
+
+
